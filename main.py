729860931037444d632e128b58d65933745fbd14
Marcos <marcosfellipeazanha@gmail.com>
2021-05-26 23:48:45 -0300

    Inserindo biblioteca
    Inserindo biblioteca de pyaudio

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # Main File
+
+import speech_recognition as sr
+
+#Cria um reconhecedor
+r = sr.Recognizer()
+
+#abrir o microfone para captura
+with sr.Microphone() as source:
+   
+    audio = r.listen(source) #Define microfone como fonte de áudio
+
+    print(r.recognize_google(audio,language="pt-BR"))
